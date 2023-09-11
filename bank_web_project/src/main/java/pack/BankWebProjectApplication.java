79b5dc2995b2a8ebd4373ab45cc99e7930ae285f
junghyeyoun <tnsdia315@naver.com>
2023-09-11 15:24:40 +0900

    first commit

---
+++ bank_web_project/src/main/java/pack/BankWebProjectApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebProjectApplication.class, args);
+	}
+
+}
