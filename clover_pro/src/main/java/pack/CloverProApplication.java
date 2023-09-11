8fefdcea1a8a8f3050194baf24c5717e46fc67f1
윤정혜 <@DESKTOP-QQCNB72>
2023-09-11 19:40:54 +0900

    first commit

--- clover_pro/src/main/java/pack/CloverProApplication.java
+++ clover_pro/src/main/java/pack/CloverProApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankWebProjectApplication {
+public class CloverProApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankWebProjectApplication.class, args);
+		SpringApplication.run(CloverProApplication.class, args);
 	}
 
 }
---
+++ clover_pro/src/main/java/pack/Test.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("제발 되라 짜증나니까");
+	}
+}
