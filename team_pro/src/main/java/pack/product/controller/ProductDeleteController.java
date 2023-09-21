0d23ee2bdde04fd05a3463f0ef35a34d603cba59
윤정혜 <@DESKTOP-QQCNB72>
2023-09-22 00:34:47 +0900

    삭제 시도는 해봤지만 안됨 - 9.22 junghye commit

--- team_pro/src/main/java/pack/product/controller/ProductDeleteController.java
+++ team_pro/src/main/java/pack/product/controller/ProductDeleteController.java
@@ -1,25 +1,26 @@
 package pack.product.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pack.product.model.ProductDao;
 
 @Controller
 public class ProductDeleteController {
-    @Autowired
-    private ProductDao productDao;
+	@Autowired
+	private ProductDao productDao;
 
-    @PostMapping("productdelete")
-    public String delete(@RequestParam("productId") int productId, @RequestParam("page") String page) {
-    	if(productDao.delete(productId))
-    		return "redirect:productlist?page="+page;
-    	else
-    		return "redirect:error";
-    }
+	@PostMapping("productdelete")
+	public String delete(RedirectAttributes rdat, @RequestParam("productId") int productId,
+			@RequestParam("page") String page) {
+		productDao.delete(productId);
+		// Redirect로 페이지 이동 시 전송값을 숨겨서 보내는 역할을 하는 클래스 : RedirectAttributes
+		rdat.addFlashAttribute("msg", "삭제");
+		return "redirect:productlist?page=" + page;
+	}
 
 }
